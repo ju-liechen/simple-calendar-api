d3aac8e49f9fdeebcfcc2e6fa411088a8200cf23
ju-liechen <juliechen4649@gmail.com>
2024-11-07 10:36:34 -0400

    Add user/my-profile endpoint

--- apps/user/api/user/my_profile/__init__.py
+++ apps/user/api/user/my_profile/__init__.py
@@ -1,26 +1,33 @@
-import uuid
+from django.contrib.auth import get_user_model
 from ninja import Schema
 from typing import Optional
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
+from uuid import UUID
 
 from project.api import api
-
 
 PATH = '/user/my-profile'
 NAME = 'user-my-profile'
 
 
 class MyProfileOut(Schema):
-    id: uuid.UUID
+    id: UUID
     email: EmailStr
+    first_name: Optional[str]
+    last_name: Optional[str]
+    is_admin: bool
 
 
 class MyProfileIn(Schema):
-    email: EmailStr
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
 
-
-class MyProfileUpdateIn(Schema):
-    email: Optional[str] = None
+    @field_validator('email')
+    def validate_email(cls, email):
+        if get_user_model().objects.filter(email__iexact=email.lower()).exists():
+            raise ValueError("User with email already exists")
+        return email.lower()
 
 
 @api.get(PATH, url_name=f"{NAME}-get", response=MyProfileOut)
@@ -29,7 +36,7 @@ def user_my_profile_get(request):
 
 
 @api.patch(PATH, url_name=f"{NAME}-patch", response=MyProfileOut)
-def user_my_profile_patch(request, data: MyProfileUpdateIn):
+def user_my_profile_patch(request, data: MyProfileIn):
     user = request.user
     for (key, value) in data.dict(exclude_unset=True).items():
         setattr(user, key, value)
